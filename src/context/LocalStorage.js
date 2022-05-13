30e00af057a4349c1a428022f8fec0f7307abbcc
eyobed <felekeeyobed@gmail.com>
2022-05-13 10:03:22 -0400

    storing users info is done

---
+++ src/context/LocalStorage.js
@@ -0,0 +1,25 @@
+
+
+export const loadState = (name) => {
+
+    try {
+        const serializedState = localStorage.getItem(name);
+        if(serializedState === null){
+            return undefined;
+        }
+        console.log(serializedState)
+        return JSON.parse(serializedState);
+    } catch (err) {
+        return undefined;
+    }
+};
+
+export const saveState = (name,state) => {
+    try{
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem(name, serializedState);
+
+    } catch (err){
+        return undefined;
+    }
+}
